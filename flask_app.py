f64f255682780510d0895dd52403fcd083b6c23f
Kent Frazier <kentfrazier@gmail.com>
2020-01-16 23:26:27 +0000

    Changed main.html body to display date

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,7 +7,7 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET'])
 def main():
     hostname = request.host_url
-    message = 'Thanks for being you!'
+    message = datetime.now()
     return render_template('main.html', host=hostname, msg=message)
 
 @app.route('/about', methods=['GET'])
